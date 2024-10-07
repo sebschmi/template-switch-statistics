6c0c155128a3feddd14b1e0156a516499cce7264
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-10-07 10:22:05 +0300

    Add basic cli.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// The statistics toml files to use for the plots.
+    #[arg()]
+    statistics_files: Vec<PathBuf>,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
+    if cli.statistics_files.is_empty() {
+        panic!("No statistics files given.");
+    }
+
     println!("Hello, world!");
 }
