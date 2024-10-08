642864326a1efeed9b72d27820406897b6042b41
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-10-08 11:44:31 +0300

    Accumulate statistics.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::{
     collections::HashMap,
     fs::File,
+    hash::Hash,
     io::Read,
     path::{Path, PathBuf},
 };
 
 use clap::Parser;
+use lib_tsalign::a_star_aligner::alignment_result::AlignmentStatistics;
+use log::info;
 use plotters::prelude::*;
-use statistics_file::StatisticsFile;
+use statistics_file::{AlignmentParameters, MergedStatisticsFile, StatisticsFile};
 
 mod statistics_file;
 
@@ -23,6 +26,14 @@ struct Cli {
 }
 
 fn main() {
+    simplelog::TermLogger::init(
+        log::LevelFilter::Info,
+        Default::default(),
+        simplelog::TerminalMode::Mixed,
+        simplelog::ColorChoice::Auto,
+    )
+    .unwrap();
+
     let cli = Cli::parse();
 
     if cli.statistics_files.is_empty() {
@@ -46,74 +57,60 @@ fn main() {
         "opened_nodes",
         (400, 400),
         &statistics_files,
-        |file| file.length,
-        |file| file.length as f64,
-        |file| file.test_sequence_name.clone(),
-        |file| file.statistics.opened_nodes as f64,
+        |file| file.parameters.length,
+        |file| file.parameters.length as f64,
+        |file| file.parameters.test_sequence_name.clone(),
+        |file| {
+            let mut parameters = file.parameters.clone();
+            parameters.seed = 0;
+            parameters
+        },
+        |statistics| statistics.opened_nodes.raw(),
     );
 }
 
 #[allow(clippy::too_many_arguments)]
-fn grouped_linear_bar_plot<SortKey: Ord>(
+fn grouped_linear_bar_plot<GroupName: Hash + Eq + ToString, SortKey: Ord>(
     output_directory: impl AsRef<Path>,
     name: impl ToString,
     size: (u32, u32),
     statistics_files: &[StatisticsFile],
-    sort_key_fn: impl Fn(&StatisticsFile) -> SortKey,
-    key_fn: impl Fn(&StatisticsFile) -> f64,
-    group_fn: impl Fn(&StatisticsFile) -> String,
-    value_fn: impl Fn(&StatisticsFile) -> f64,
+    sort_key_fn: impl Fn(&MergedStatisticsFile) -> SortKey,
+    key_fn: impl Fn(&MergedStatisticsFile) -> f64,
+    group_name_fn: impl Fn(&StatisticsFile) -> GroupName,
+    merge_key_fn: impl Fn(&StatisticsFile) -> AlignmentParameters,
+    value_fn: impl Fn(&AlignmentStatistics) -> f64,
 ) {
-    let mut groups: HashMap<_, Vec<_>> = Default::default();
-
-    for file in statistics_files {
-        let group_name = group_fn(file);
-        if let Some(group) = groups.get_mut(&group_name) {
-            group.push(file.clone());
-        } else {
-            groups.insert(group_name, vec![file.clone()]);
-        }
-    }
-
-    groups
-        .values_mut()
-        .for_each(|group| group.sort_unstable_by_key(&sort_key_fn));
-
-    assert!(
-        groups
-            .values()
-            .skip(1)
-            .fold(
-                (groups.values().next().unwrap().len(), true),
-                |(len, truth), group| (len, truth && group.len() == len)
-            )
-            .1,
-        "groups are not of equal size"
-    );
+    let groups = group_files(statistics_files, group_name_fn);
+    let groups = merge_files_in_groups(groups, merge_key_fn);
+    let groups = sort_groups(groups, sort_key_fn);
 
     let mut output_file_name = name.to_string();
     output_file_name.push_str(".svg");
     let mut output_file = output_directory.as_ref().to_owned();
     output_file.push(output_file_name);
 
-    let (min_key, max_key) =
-        statistics_files
-            .iter()
-            .map(&key_fn)
-            .fold((f64::INFINITY, 0.0), |(min, max), value| {
-                (
-                    if min < value { min } else { value },
-                    if max > value { max } else { value },
-                )
-            });
-    let max_value = statistics_files
-        .iter()
-        .map(&value_fn)
+    let (min_key, max_key) = groups
+        .values()
+        .flat_map(|group| group.iter())
+        .map(&key_fn)
+        .fold((f64::INFINITY, 0.0), |(min, max), value| {
+            (
+                if min < value { min } else { value },
+                if max > value { max } else { value },
+            )
+        });
+    let max_value = groups
+        .values()
+        .flat_map(|group| group.iter())
+        .map(|file| value_fn(&file.max_statistics))
         .fold(0.0, |acc, value| if acc < value { value } else { acc });
 
+    info!("Creating drawing area");
     let root = SVGBackend::new(&output_file, size).into_drawing_area();
     root.fill(&TRANSPARENT).unwrap();
 
+    info!("Creating chart context with key range {min_key}..{max_key} and max value {max_value}");
     let mut chart = ChartBuilder::on(&root)
         .caption(name.to_string(), ("sans-serif", 50).into_font())
         .margin(5)
@@ -122,6 +119,7 @@ fn grouped_linear_bar_plot<SortKey: Ord>(
         .build_cartesian_2d(min_key / 1.05..max_key * 1.05, 0.0..max_value * 1.05)
         .unwrap();
 
+    info!("Configuring chart mesh");
     chart
         .configure_mesh()
         .disable_x_mesh()
@@ -135,7 +133,11 @@ fn grouped_linear_bar_plot<SortKey: Ord>(
         .iter()
         .zip([&RED, &GREEN, &BLUE, &MAGENTA, &CYAN, &YELLOW])
     {
-        let coordinate_iterator = group.iter().map(&key_fn).zip(group.iter().map(&value_fn));
+        info!("Drawing group {}", group_name.to_string());
+        let coordinate_iterator = group
+            .iter()
+            .map(&key_fn)
+            .zip(group.iter().map(|file| value_fn(&file.mean_statistics)));
 
         chart
             .draw_series(LineSeries::new(coordinate_iterator.clone(), style))
@@ -143,7 +145,7 @@ fn grouped_linear_bar_plot<SortKey: Ord>(
         chart
             .draw_series(coordinate_iterator.map(|coordinate| Circle::new(coordinate, 3, style)))
             .unwrap()
-            .label(group_name)
+            .label(group_name.to_string())
             .legend(move |(x, y)| Rectangle::new([(x - 5, y - 5), (x + 5, y + 5)], style));
     }
 
@@ -153,6 +155,85 @@ fn grouped_linear_bar_plot<SortKey: Ord>(
         .border_style(BLACK)
         .draw()
         .unwrap();
+}
+
+fn group_files<GroupName: Hash + Eq>(
+    statistics_files: &[StatisticsFile],
+    group_name_fn: impl Fn(&StatisticsFile) -> GroupName,
+) -> HashMap<GroupName, Vec<StatisticsFile>> {
+    info!("Grouping files");
+
+    let mut groups: HashMap<_, Vec<_>> = Default::default();
+
+    for file in statistics_files {
+        let group_name = group_name_fn(file);
+        if let Some(group) = groups.get_mut(&group_name) {
+            group.push(file.clone());
+        } else {
+            groups.insert(group_name, vec![file.clone()]);
+        }
+    }
+
+    assert!(
+        groups
+            .values()
+            .skip(1)
+            .fold(
+                (groups.values().next().unwrap().len(), true),
+                |(len, truth), group| (len, truth && group.len() == len)
+            )
+            .1,
+        "groups are not of equal size"
+    );
+
+    info!(
+        "Created {} groups with {} elements each",
+        groups.len(),
+        groups.values().next().unwrap().len()
+    );
+
+    groups
+}
+
+fn merge_files_in_groups<GroupName: Hash + Eq>(
+    groups: HashMap<GroupName, Vec<StatisticsFile>>,
+    merge_key_fn: impl Fn(&StatisticsFile) -> AlignmentParameters,
+) -> HashMap<GroupName, Vec<MergedStatisticsFile>> {
+    info!("Merge files in groups");
+
+    let mut merged_groups = HashMap::new();
+
+    for (group_name, group) in groups {
+        let mut merged_group: HashMap<_, Vec<_>> = Default::default();
+
+        for file in group {
+            let merge_key = merge_key_fn(&file);
+            if let Some(statistics) = merged_group.get_mut(&merge_key) {
+                statistics.push(file);
+            } else {
+                merged_group.insert(merge_key, vec![file]);
+            }
+        }
+
+        merged_groups.insert(
+            group_name,
+            merged_group.into_values().map(Into::into).collect(),
+        );
+    }
+
+    merged_groups
+}
+
+fn sort_groups<GroupName: Hash + Eq, SortKey: Ord, StatisticsType>(
+    mut groups: HashMap<GroupName, Vec<StatisticsType>>,
+    sort_key_fn: impl Fn(&StatisticsType) -> SortKey,
+) -> HashMap<GroupName, Vec<StatisticsType>> {
+    info!("Sort groups");
+
+    groups
+        .values_mut()
+        .for_each(|group| group.sort_unstable_by_key(&sort_key_fn));
+    groups
 }
 
 fn format_value(value: &f64) -> String {
--- src/statistics_file.rs
+++ src/statistics_file.rs
@@ -1,16 +1,67 @@
 use lib_tsalign::a_star_aligner::{
-    alignment_result::AlignmentResult, template_switch_distance::AlignmentType,
+    alignment_result::{AlignmentResult, AlignmentStatistics},
+    template_switch_distance::AlignmentType,
 };
+use noisy_float::types::R64;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize)]
 pub struct StatisticsFile {
     #[serde(flatten)]
     pub statistics: AlignmentResult<AlignmentType>,
 
+    #[serde(flatten)]
+    pub parameters: AlignmentParameters,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+pub struct AlignmentParameters {
     pub test_sequence_name: String,
     pub length: usize,
     pub seed: u64,
     pub alignment_config: String,
     pub ts_node_ord_strategy: String,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub struct MergedStatisticsFile {
+    pub min_statistics: AlignmentStatistics,
+    pub max_statistics: AlignmentStatistics,
+    pub mean_statistics: AlignmentStatistics,
+    pub median_statistics: AlignmentStatistics,
+
+    pub parameters: AlignmentParameters,
+}
+
+impl From<Vec<StatisticsFile>> for MergedStatisticsFile {
+    fn from(statistics_files: Vec<StatisticsFile>) -> Self {
+        assert!(!statistics_files.is_empty());
+
+        let mut result = Self {
+            min_statistics: AlignmentStatistics::max_value(),
+            max_statistics: AlignmentStatistics::min_value(),
+            mean_statistics: AlignmentStatistics::zero(),
+            median_statistics: AlignmentStatistics::piecewise_percentile(
+                &statistics_files
+                    .iter()
+                    .map(|file| file.statistics.statistics.clone())
+                    .collect::<Vec<_>>(),
+                R64::new(0.5),
+            ),
+            parameters: statistics_files[0].parameters.clone(),
+        };
+
+        for statistics in &statistics_files {
+            let statistics = &statistics.statistics.statistics;
+            result.min_statistics = result.min_statistics.piecewise_min(statistics);
+            result.max_statistics = result.max_statistics.piecewise_max(statistics);
+            result.mean_statistics = result.mean_statistics.piecewise_add(statistics);
+        }
+
+        result.mean_statistics = result
+            .mean_statistics
+            .piecewise_div(R64::new(statistics_files.len() as f64));
+
+        result
+    }
 }
